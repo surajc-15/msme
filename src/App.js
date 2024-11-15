2e0600c415f1d8bef520ca7710aa3864f4bd7847
surajc-15 <surajchanaveeragoudra@gmail.com>
2024-11-15 16:23:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <First />
+      <Second />
+      <Third />
+      <Fourth />
+      <Fifth />
+      <Sixth />
+      <Seventh />
+      <Eighth />
+      <Ningth />
+      <Tenth />
+    </div>
+  );
+}
+
+function First() {
+  return <h1>hello namaste</h1>;
+}
+
+function Second() {
+  return <h1>I am suraj</h1>;
+}
+function Third() {
+  return <h1>3rd year student</h1>;
+}
+function Fourth() {
+  return <h1>Learning React</h1>;
+}
+function Fifth() {
+  return <h1>MERN stack is crazy</h1>;
+}
+function Sixth() {
+  return <h1>i love development</h1>;
+}
+function Seventh() {
+  return <h1> Every one is good at what they do</h1>;
+}
+function Eighth() {
+  return <h1>focus on learning </h1>;
+}
+function Ningth() {
+  return <h1>Thank you</h1>;
+}
+
+function Tenth() {
+  return <h2 id="tenth">~suraj</h2>;
+}
+
+export default App;
